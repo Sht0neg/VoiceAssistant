c0c0efa3be740992ae7948031c93464d1066350f
Academy <aper29012006@gmail.com>
2025-11-24 20:55:46 +0300

    Добавлены функции открытия туба и поиск в браузере

--- skills.py
+++ skills.py
@@ -2,27 +2,27 @@ import sys
 import os
 import time
 from pathlib import Path
+import webbrowser
+import requests
+import json
 
+def video():
+     webbrowser.open("https://www.youtube.com", new=2)
+
+def search(data):
+     webbrowser.open(f"https://www.google.com/search?q={data}")
+
+def calc():
+     os.system('calc')
 
 def find_exe(filename):
-    start = time.time()
     for driver in os.listdrives():
         for root, _, files in os.walk(driver):
             for file in files:
                 if filename == file and file.endswith(".exe") and "$Recycle.Bin" not in root:
                         return os.path.join(root, file)
-    end = time.time()
-    print(start - end)
     return 
 
-
-def find2_exe(filename):
-    start = time.time()
-    for driver in os.listdrives():
-        print(os.system(f'where /r {driver} "{filename}"'))
-    end = time.time()
-    print(start - end)
-    return 
 
 def find3_exe(filename):
     for driver in os.listdrives():
@@ -41,5 +41,3 @@ def offBot():
 def passive():
 	pass
 
-
-print(find3_exe("Tanki.exe"))
