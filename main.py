41be9720a8d28138b7a23c3b72eea2c995a56f18
megatrc <Sh0tnik.19@yandex.ru>
2026-03-02 19:54:30 +0300

    изменение главное файла, создание главного класса по приципу singltone, функции подключения модели и добавления устройств

--- main.py
+++ main.py
@@ -14,11 +14,73 @@ import sys
 from datetime import datetime
 
 
+assistant_running = False
+assistant_thread = None
+current_settings = {
+    'volume': 70,
+    'input_device': None,
+    'output_device': None,
+    'window_size': '600x700'
+}
 q = queue.Queue()
-
 model = vosk.Model("model")
 
-device = sd.default.device
+class VoiceAssistantAPI:
+    def __init__(self):
+        self.sample_rate = 16000 
+        self.current_volume = 0.7
+        self.input_stream = None
+        self.output_stream = None
+        self.is_streaming = False
+        self.input_device = None
+        self.output_device = None
+        self.vosk_model = None
+        self.rec = None
+        
+        self.load_vosk_model()
+        
+        self.refresh_devices()
+
+    def load_vosk_model(self):
+        try:
+            model_path = "model"
+            if os.path.exists(model_path):
+                self.vosk_model = vosk.Model(model_path)
+        except Exception as e:
+            pass
+
+    def refresh_devices(self):
+        try:
+            self.devices = sd.query_devices()
+            self.input_devices = []
+            self.output_devices = []
+            
+            for i, device in enumerate(self.devices):
+                if device['max_input_channels'] > 0:
+                    self.input_devices.append({
+                        'index': i,
+                        'name': device['name'],
+                        'channels': device['max_input_channels'],
+                        'samplerate': device['default_samplerate']
+                    })
+                if device['max_output_channels'] > 0:
+                    self.output_devices.append({
+                        'index': i,
+                        'name': device['name'],
+                        'channels': device['max_output_channels'],
+                        'samplerate': device['default_samplerate']
+                    })
+        except Exception as e:
+            self.input_devices = []
+            self.output_devices = []
+    
+    def get_devices(self):
+        self.refresh_devices()
+        return {
+            'input': self.input_devices,
+            'output': self.output_devices
+        }
+
 samplerate = int(sd.query_devices(device[0], "input")["default_samplerate"])
 
 def callback(indata, frames, time, status):
